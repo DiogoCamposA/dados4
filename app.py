3ee4a8e7e4c03feca797c1dd20980a47ba495478
DiogoCamposA <147980640+DiogoCamposA@users.noreply.github.com>
2023-10-15 17:11:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import paho.mqtt.client as mqtt
 import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,22 +10,32 @@ MQTT_BROKER_HOST = "broker.hivemq.com"
 MQTT_BROKER_PORT = 1883
 MQTT_TOPIC = "MQTTINCBTempUmidDiogo"
 
-# Variável global para armazenar a última mensagem do tópico
-last_message = ""
-
 # Configuração do banco de dados SQLite
 DB_NAME = "mqtt_data.db"
 
 def create_table():
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
+    
+    # Criar nova tabela se não existir
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS messages (
+        CREATE TABLE IF NOT EXISTS messages_new (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             topic TEXT,
-            payload TEXT
+            payload TEXT,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
         )
     ''')
+    
+    # Copiar dados da tabela antiga para a nova
+    cursor.execute('INSERT INTO messages_new (topic, payload) SELECT topic, payload FROM messages')
+    
+    # Remover a tabela antiga
+    cursor.execute('DROP TABLE IF EXISTS messages')
+    
+    # Renomear a nova tabela para o nome desejado
+    cursor.execute('ALTER TABLE messages_new RENAME TO messages')
+    
     conn.commit()
     conn.close()
 
@@ -40,8 +51,8 @@ def insert_message(topic, payload):
 def get_messages():
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM messages ORDER BY id DESC LIMIT 1')
-    result = cursor.fetchone()
+    cursor.execute('SELECT * FROM messages ORDER BY timestamp DESC')
+    result = cursor.fetchall()
     conn.close()
     return result
 
@@ -50,12 +61,9 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe(MQTT_TOPIC)
 
 def on_message(client, userdata, msg):
-    global last_message
-    last_message = msg.payload.decode()
-    print("Mensagem recebida do tópico {}: {}".format(MQTT_TOPIC, last_message))
-    
-    # Inserir a mensagem no banco de dados
-    insert_message(MQTT_TOPIC, last_message)
+    # Armazena cada mensagem no banco de dados com carimbo de data/hora
+    insert_message(msg.topic, msg.payload.decode())
+    print("Mensagem recebida do tópico {}: {}".format(msg.topic, msg.payload.decode()))
 
 # Configurar o cliente MQTT
 mqtt_client = mqtt.Client()
@@ -69,12 +77,12 @@ create_table()
 # Iniciar a thread do cliente MQTT
 mqtt_client.loop_start()
 
-# Rota principal que renderiza a última mensagem do tópico
+# Rota principal que renderiza todas as mensagens do tópico
 @app.route('/')
 def index():
-    # Obter a última mensagem do banco de dados
-    message_from_db = get_messages()
-    return render_template('index.html', message=message_from_db)
+    # Obter todas as mensagens do banco de dados
+    messages_from_db = get_messages()
+    return render_template('index.html', messages=messages_from_db)
 
 if __name__ == '__main__':
     app.run(debug=True)
